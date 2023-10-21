d440a0c4b25ee07c1c6e54bdb52541faf8082c45
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-10-21 15:16:51 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleyUlElement = document.querySelector("ul.gallery");
+
+const imagesMarkup = galleryItems.reduce((acc, img) => {
+  const singleImgMarkup = `
+     <li class="gallery__item">
+      <a class="gallery__link" href="${img.original}">
+          <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+      </a>
+    </li>
+  `;
+
+  return acc + singleImgMarkup;
+}, "");
+
+galleyUlElement.insertAdjacentHTML("afterbegin", imagesMarkup);
+
+const lightbox = new SimpleLightbox("ul.gallery a.gallery__link", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+});
